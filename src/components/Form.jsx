076e8d9b4f9eabfcc2fbbc6eee0e077f2b592874
Michael Holmes <michaelholmes618@gmail.com>
2021-04-22 17:22:42 -0500

    Complete Challenge Operators in Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Input from "./Input";
 
-function Form() {
+function Form(props) {
   return (
     <form className="form">
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      {!props.isRegistered && (
+        <Input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
     </form>
   );
 }
